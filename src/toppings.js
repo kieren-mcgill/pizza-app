ec8a7cc16233effec359806174d43c89cb31f9ff
Andrew Charlesworth <andrewcharlesworth@andysmac.local>
2023-04-19 16:12:48 +0100

    Added toppings and bases static data

---
+++ src/toppings.js
@@ -0,0 +1,77 @@
+export const toppings =  Object.freeze({
+  tomato: Object.freeze({
+    label: "Tomatoes"
+  }),
+  cheddar: Object.freeze({
+    label: "Cheddar"
+  }),
+  mozzarella: Object.freeze({
+    label: "Mozzarella"
+  }),
+  parmesan: Object.freeze({
+    label: "Parmesan"
+  }),
+  feta: Object.freeze({
+    label: "Feta"
+  }),
+  stilton: Object.freeze({
+    label: "Stilton"
+  }),
+  mushrooms: Object.freeze({
+    label: "Mushrooms"
+  }),
+  anchovies: Object.freeze({
+    label: "Anchovies"
+  }),
+  jalapeno: Object.freeze({
+    label: "Jalapeno"
+  }),
+  salami: Object.freeze({
+    label: "Salami"
+  }),
+  Pepperoni: Object.freeze({
+    label: "Pepperoni"
+  }),
+  ham: Object.freeze({
+    label: "Ham"
+  }),
+  chicken: Object.freeze({
+    label: "Chicken"
+  }),
+  sweetcorn: Object.freeze({
+    label: "Sweetcorn"
+  }),
+  tuna: Object.freeze({
+    label: "Tuna"
+  }),
+  artichoke: Object.freeze({
+    label: "Artichoke"
+  }),
+});
+
+export const bases = Object.freeze({
+  largeDeepPan: Object.freeze({
+    label: "Large Deep Pan",
+  }),
+  mediumDeepPan: Object.freeze({
+    label: "Medium Deep Pan",
+  }),
+  largeThin: Object.freeze({
+    label: "Large Thin",
+  }),
+  mediumThin: Object.freeze({
+    label: "Medium Thin",
+  }),
+  largeGf: Object.freeze({
+    label: "Large Gluten Free",
+  }),
+  mediumGf: Object.freeze({
+    label: "Medium Gluten Free",
+  }),
+  largeStuffedCrust: Object.freeze({
+    label: "Large Stuffed Crust",
+  }),
+  mediumStuffedCrust: Object.freeze({
+    label: "Medium Stuffed Crust",
+  }),
+});
