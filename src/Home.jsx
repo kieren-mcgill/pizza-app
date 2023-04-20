5630ae3e30266972b8b15fee1fdb9e32e89990b5
Eyup Student <eyupstudent@EyUps-MacBook-Air.local>
2023-04-20 11:46:59 +0100

    created pizza array and routed to pizza form

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,9 +2,16 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./Header";
 import CreateButton from "./CreateButton";
 import BasketSummary from "./BasketSummary";
+import { useState } from "react";
 
 
 const Home = () => {
+
+  const [pizzaArray, newPizzaArray] = useState([])
+
+  const addPizza = (pizza) => {
+    newPizzaArray([...pizzaArray, pizza])
+  }
 
 
   return (
@@ -17,6 +24,7 @@ const Home = () => {
           <Route path="/" element={(<CreateButton/>)}/>
           <Route path="/basket" element={(<BasketSummary/>)}/>
           <Route path="*" element={<p>Page Not Found</p>}/>
+          <Route path="/pizza-form" element={(<PizzaForm pizzaArray={pizzaArray} addPizza={addPizza}/>)}/>
         </Routes>
       </main>
     </>
